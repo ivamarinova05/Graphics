a610026bd129d3f6b2f3497ee20c6c01d24cb9f9
ivamarinova05 <62331307+ivamarinova05@users.noreply.github.com>
2020-05-31 23:16:22 +0300

    Create Functions.h

---
+++ Functions.h
@@ -0,0 +1,14 @@
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+#include <string>
+#include <vector>
+
+class Functions
+{
+    public:
+    //за правилното извеждане във файл
+    static bool oneDigit(int);
+    static bool twoDigit(int);
+
+};
+#endif
