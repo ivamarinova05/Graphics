7d83464a3a4af296a684aba8eb1bf651e22a7a6c
ivamarinova05 <62331307+ivamarinova05@users.noreply.github.com>
2020-05-31 23:17:48 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include "RGB.h"
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
+#include "PPM.h"
+#include "PBM.h"
+#include "CLR.h"
+#include "Session.h"
+
+int main ()
+{
+    CLR commandLineReader;
+    commandLineReader.read();
+    
+    
+    
+    return 0;
+}
